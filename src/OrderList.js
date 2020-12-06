d20726d79f4a88c682c1ddaaeea9dd72da6f889e
vsnikalex <vasilii.nikolaev@icloud.com>
2020-12-06 15:11:05 +0300

    Add static progress bars

--- src/OrderList.js
+++ src/OrderList.js
@@ -1,16 +1,31 @@
 import React from 'react';
+import OrderState from "./OrderState";
 
 function OrderList() {
 
     return (
         <div className="container-fixed">
             <h1 className="underline">Orders</h1>
-            <table className="table table-hover">
-                <tr>
-                    <td>12345</td>
-                    <td>Order Progress Bar</td>
-                </tr>
-            </table>
+            <ul className="content-list">
+                <li className="media">
+                    <div className="media-body">
+                        <div className="media-heading">Vasilii</div>
+                        <div className="media-hint">Payment Accepted</div>
+                    </div>
+                </li>
+                <li className="content-list-item">
+                    <OrderState />
+                </li>
+                <li className="media">
+                    <div className="media-body">
+                        <div className="media-heading">Vasilii</div>
+                        <div className="media-hint">Customer Verified</div>
+                    </div>
+                </li>
+                <li className="content-list-item">
+                    <OrderState />
+                </li>
+            </ul>
         </div>
     );
 }
