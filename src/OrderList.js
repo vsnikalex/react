db81b2a514c76c401ba528d205b2449e601e9680
vsnikalex <vasilii.nikolaev@icloud.com>
2020-12-06 14:33:45 +0300

    Add DT styles (css loading config required)

---
+++ src/OrderList.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function OrderList() {
+
+    return (
+        <div className="container-fixed">
+            <h1 className="underline">Orders</h1>
+            <table className="table table-hover">
+                <tr>
+                    <td>12345</td>
+                    <td>Order Progress Bar</td>
+                </tr>
+            </table>
+        </div>
+    );
+}
+
+export default OrderList;
