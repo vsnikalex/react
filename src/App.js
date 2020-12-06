dd2039fdf2902dd68ed1eef7b11b0f3e5eb7fc33
vsnikalex <vasilii.nikolaev@icloud.com>
2020-12-06 11:00:47 +0300

    Add conditional rendering with login button

--- src/App.js
+++ src/App.js
@@ -7,7 +7,10 @@ class App extends React.Component {
     constructor(props) {
         super(props);
         // set initial state in constructor
-        this.state = {date: new Date()};
+        this.state = {
+            date: new Date(),
+            isLoggedIn: false
+        };
     }
 
     componentDidMount() {
@@ -28,18 +31,69 @@ class App extends React.Component {
         });
     }
 
+    handleLoginClick = () => {
+        this.setState(() => ({isLoggedIn: true}));
+    }
+
+    handleLogoutClick = () => {
+        this.setState(() => ({isLoggedIn: false}));
+    }
+
     render() {
+        const isLoggedIn = this.state.isLoggedIn;
+        let button;
+        if (isLoggedIn) {
+            button = <LogoutButton onClick={this.handleLogoutClick} />;
+        } else {
+            button = <LoginButton onClick={this.handleLoginClick} />;
+        }
+
         return (
             <div className="App">
                 <header className="App-header">
                     <img src={logo} className="App-logo" alt="logo"/>
-                    <h1>Hello, {this.props.name}!</h1>
+                    <div>
+                        <Greeting isLoggedIn={isLoggedIn} name={this.props.name}/>
+                        {button}
+                    </div>
                     <h2>It is <code>{this.state.date.toLocaleTimeString()}</code></h2>
                     <Toggle />
                 </header>
             </div>
         );
     }
+}
+
+function UserGreeting(props) {
+    return <h1>Welcome back {props.name}!</h1>;
+}
+
+function GuestGreeting() {
+    return <h1>Please sign up.</h1>;
+}
+
+function Greeting(props) {
+    const isLoggedIn = props.isLoggedIn;
+    if (isLoggedIn) {
+        return <UserGreeting name={props.name}/>;
+    }
+    return <GuestGreeting />;
+}
+
+function LoginButton(props) {
+    return (
+        <button onClick={props.onClick}>
+            Login
+        </button>
+    );
+}
+
+function LogoutButton(props) {
+    return (
+        <button onClick={props.onClick}>
+            Logout
+        </button>
+    );
 }
 
 export default App;
