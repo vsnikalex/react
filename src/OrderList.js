8aa741058b621701cba5d035a411351ae6f84d65
vsnikalex <vasilii.nikolaev@icloud.com>
2020-12-13 22:31:36 +0300

    Store new Orders in state

--- src/OrderList.js
+++ src/OrderList.js
@@ -2,15 +2,32 @@ import React from 'react';
 import Order from "./Order";
 
 class OrderList extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            orders: []
+        };
+    }
 
     componentDidMount() {
         const source = new EventSource("http://localhost:8080/events/newOrders", { withCredentials: true } );
-        source.onmessage = function (event) {
-            console.log(JSON.parse(event.data))
+        source.onmessage = event => {
+            console.log('New order created: ', JSON.parse(event.data))
+
+            this.setState(state => {
+                const orders = state.orders.concat(JSON.parse(event.data));
+
+                return {
+                    orders
+                };
+            });
         };
     }
 
     render() {
+        console.log('List of orders: ', this.state.orders)
+
         return (
             // TODO: subscribe on SSE about new Orders,
             //  append new order each time
