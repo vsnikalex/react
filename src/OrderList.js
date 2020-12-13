00dc1a4309cd659f2903dc80ac6a18b834f656e0
vsnikalex <vasilii.nikolaev@icloud.com>
2020-12-13 18:30:03 +0300

    Log information about new Orders to console

--- src/OrderList.js
+++ src/OrderList.js
@@ -2,6 +2,14 @@ import React from 'react';
 import Order from "./Order";
 
 class OrderList extends React.Component {
+
+    componentDidMount() {
+        const source = new EventSource("http://localhost:8080/events/newOrders", { withCredentials: true } );
+        source.onmessage = function (event) {
+            console.log(JSON.parse(event.data))
+        };
+    }
+
     render() {
         return (
             // TODO: subscribe on SSE about new Orders,
